fb3fce6ab04a956051516aa8217e35f90ac7fede
warmachine028 <warmachine028@outlook.com>
2022-06-15 13:10:41 +0530

    Initial Commit

---
+++ components/Button.js
@@ -0,0 +1,44 @@
+import { TouchableOpacity, Text, Image } from "react-native"
+import { COLORS, SIZES, FONTS, SHADOWS } from "../constants"
+
+export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
+	return (
+		<TouchableOpacity
+			style={{
+				width: 40,
+				height: 40,
+				backgroundColor: COLORS.white,
+				position: "absolute",
+				borderRadius: SIZES.extraLarge,
+				alignItems: "center",
+				justifyContent: "center",
+				...SHADOWS.light,
+				...props,
+			}}
+			onPress={handlePress}
+		>
+			<Image source={imgUrl} resizeMode="contain" style={{ width: 24, height: 24 }} />
+		</TouchableOpacity>
+	)
+}
+
+export const RectButton = ({ minWidth, fontSize, handlePress, ...props }) => {
+	return (
+		<TouchableOpacity
+			style={{
+				backgroundColor: COLORS.primary,
+				padding: SIZES.small,
+				borderRadius: SIZES.extraLarge,
+				minWidth: minWidth,
+				...props,
+			}}
+			onPress={handlePress}
+		>
+			<Text
+				style={{ fontSize: fontSize, fontFamily: FONTS.semiBold, color: COLORS.white, textAlign: "center", }}
+			>
+				Place a bid
+			</Text>
+		</TouchableOpacity>
+	)
+}
---
+++ components/index.js
@@ -0,0 +1,9 @@
+import HomeHeader from "./HomeHeader"
+import FocusedStatusBar from "./FocusedStatusBar"
+import NFTCard from "./NFTCard"
+import { CircleButton, RectButton } from "./Button"
+import DetailsBid from "./DetailsBid"
+import DetailsDesc from "./DetailsDesc"
+import { SubInfo } from "./SubInfo"
+
+export { HomeHeader, FocusedStatusBar, NFTCard, CircleButton, RectButton, DetailsBid, DetailsDesc, SubInfo }
